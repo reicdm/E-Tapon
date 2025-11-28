1976a1bc20efe22e053df74955d76219fac3d349
Sarah <sarahmaeharina@gmail.com>
2025-11-29 00:52:19 +0800

    aaaaaaaa

--- E-Tapon/resources/views/collector/dashboard.blade.php
+++ E-Tapon/resources/views/collector/dashboard.blade.php
@@ -9,11 +9,11 @@
         <h2 class="font-extrabold" style="color: var(--color-dark-green) ">Welcome, Sample Name!</h2>
         <p style="color: var(--color-dark-green) ">ID: 123-456-789</p>
 
-        <div class="row row-cols-2 row-cols-md-2 g-2 pt-2">
+        <div class="row row-cols-2 row-cols-md-2 g-2">
             <div class="col">
-                <div class="card">
+                <div class="card-top">
                     <div class="card-body">
-                        <h6 class="card-title">Assigned Area</h6>
+                        <h6 class="card-top-title">Assigned Area</h6>
                         <div class="card-data-box-area">
                             <p class="card-text">Area: Barangay</p>
                         </div>
@@ -21,13 +21,62 @@
                 </div>
             </div>
             <div class="col">
-                <div class="card">
+                <div class="card-top">
                     <div class="card-body">
-                        <h5 class="card-title">Assigned Truck</h5>
+                        <h5 class="card-top-title">Assigned Truck</h5>
                         <div class="card-data-box-truck">
                             <p class="card-text">Plate no.: ABC 1234</p>
                             <p class="card-text">Capacity: ____</p>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row g-2">
+            <div class="col">
+                <div class="card-mid">
+                    <h2 class="font-extrabold mb-2" style="color: var(--color-dark-green)">Collection Schedule</h2>
+                    <div class="row row-cols-3 g-2">
+                        <a href="{{ route('collector.schedule') }}" class="card-link">
+                            <div class="col">
+                                <div class="card-data-box-sched">
+                                    <h2 class="card-sched-text-date">JAN 01</h2>
+                                    <div>
+                                        <p class="card-sched-text-ba">Brgy. 123</p>
+                                        <p class="card-sched-text-ba">ABC 1234</p>
+                                    </div>
+                                    <div class="card-sched-status">
+                                        <p class="sched-status-pending">Pending</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </a>
+                        <div class="col">
+                            <div class="card-data-box-sched">
+                                <h2 class="card-sched-text-date">JAN 01</h2>
+                                <p class="card-sched-text-ba">Brgy. 123</p>
+                                <p class="card-sched-text-ba">ABC 1234</p>
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="card-data-box-sched">
+                                <h2 class="card-sched-text-date">JAN 01</h2>
+                                <p class="card-sched-text-ba">Brgy. 123</p>
+                                <p class="card-sched-text-ba">ABC 1234</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row g-2">
+            <div class="col">
+                <div class="card-bot">
+                    <h2 class="font-extrabold" style="color: var(--color-light-olive) ">Request to Approve</h2>
+                    <div class="card-sched">
+
                     </div>
                 </div>
             </div>
